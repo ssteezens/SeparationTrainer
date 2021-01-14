03362a9f07e0715f4cbd7f29b4473f09e779571d
Sam <samstevens.ohio@gmail.com>
2021-01-14 13:02:47 -0500

    - made floating action buttons bigger - started stopwatchservice to keep timer alive in background when running

---
+++ SeparationTrainer/SeparationTrainer.Android/Services/DisplayStopwatchService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Util;
+
+namespace SeparationTrainer.Droid.Services
+{
+    [Service]
+    public class DisplayStopwatchService : Service
+    {
+        private bool _isStarted;
+
+        public override IBinder OnBind(Intent intent)
+        {
+            return null;
+        }
+
+        public override void OnCreate()
+        {
+            base.OnCreate();
+        }
+
+        public override StartCommandResult OnStartCommand(Intent intent, StartCommandFlags flags, int startId)
+        {
+
+            return StartCommandResult.Sticky;
+        }
+    }
+}
