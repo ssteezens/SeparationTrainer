1743e5c31423b2711916391e6660135037bb8fb4
Sam <samstevens.ohio@gmail.com>
2021-04-25 18:47:11 -0400

    - adding default message when there are no activities - adding opacity to text according to material design

---
+++ SeparationTrainer/SeparationTrainer/Converters/IntegerToVisibilityConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SeparationTrainer.Converters
+{
+    public class IntegerToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is int intValue))
+                throw new ArgumentException("Value passed to converter must be an int");
+
+            return intValue > 0;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
